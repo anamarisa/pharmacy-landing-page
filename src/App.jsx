6460c92f4d0db2255739f7574dce437fda91d72d
Ana Marisa <ana.marisa.mf@gmail.com>
2025-04-27 21:04:49 +0700

    add suspense and lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import { Routes, Route, Router } from "react-router-dom";
-import "./index.css";
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import { cards } from "./data/blogData";
-import PrepPage from "./components/PrEPPage";
-import Blog from "./components/Blog/BlogPost";
-import Post from "./components/Blog/Post";
-import LandingPage from "./components/LandingPage";
 import ScrollToTop from "./components/ScrollToTop";
+import "./index.css";
+
+const LandingPage = lazy(() => import("./components/LandingPage"));
+const Blog = lazy(() => import("./components/Blog/BlogPost"));
+const Post = lazy(() => import("./components/Blog/Post"));
+const PrepPage = lazy(() => import("./components/PrEPPage"));
 
 export default function App() {
   return (
     <>
-      <ScrollToTop /> {/* now inside BrowserRouter */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/post/:slug" element={<Post cards={cards} />} />
-        <Route path="/sti-awareness/:categoryName" element={<PrepPage />} />
-      </Routes>
+      <ScrollToTop />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/post/:slug" element={<Post cards={cards} />} />
+          <Route path="/sti-awareness/:categoryName" element={<PrepPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
